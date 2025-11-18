03142609db7a4c40fb21f2e921dc7b95721565dc
Animesh478 <animeshmohanty.am@gmail.com>
2025-11-18 23:07:00 +0530

    created user schema

--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,17 @@
-import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { name } from "ejs";
 
 export const shortLinkModel = mysqlTable("short_links", {
   id: int().autoincrement().primaryKey(),
   url: varchar({ length: 255 }).notNull(),
   shortCode: varchar("short_code", { length: 255 }).notNull().unique(),
+});
+
+export const userModel = mysqlTable("users", {
+  id: int().autoincrement().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
 });
