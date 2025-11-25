8ff90e6926a6d69b89ee1fc955940cbfe91c205f
Animesh478 <animeshmohanty.am@gmail.com>
2025-11-25 21:59:54 +0530

    handling errors with connect-flash and session

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
+import session from "express-session";
+import flash from "connect-flash";
+
 import { shortenerRouter } from "./routes/shortener.routes.js";
 import { env } from "./config/env.js";
 import { authRoutes } from "./routes/auth.routes.js";
@@ -10,6 +13,14 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret_key",
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
 app.use(verifyAuthentication);
 app.use((req, res, next) => {
   res.locals.user = req.user;
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const getRegisterPage = function (req, res) {
   if (req.user) {
     return res.redirect("/");
   }
-  res.render("auth/register");
+  res.render("auth/register", { error: req.flash("error") });
 };
 
 //? get the data from the client and create a new user in the db
@@ -26,6 +26,7 @@ export const postRegister = async function (req, res) {
 
   // if a user with that email exists, then that user has to register again with another email
   if (existingUser) {
+    req.flash("error", "User already exists");
     return res.redirect("/register");
   }
 
@@ -41,7 +42,7 @@ export const getLoginPage = function (req, res) {
   if (req.user) {
     return res.redirect("/");
   }
-  res.render("auth/login");
+  res.render("auth/login", { error: req.flash("error") });
 };
 
 export const postLogin = async function (req, res) {
@@ -54,12 +55,14 @@ export const postLogin = async function (req, res) {
 
   // if a user with that email does not exists, then that user has to login again with the correct email
   if (!user) {
+    req.flash("error", "Invalid email or password");
     return res.redirect("/login");
   }
 
   // if the password entered by the user doesnot match with the one in the db, then also the user has to login again with correct credentials
   const validPassword = await comparePassword(password, user.password);
   if (!validPassword) {
+    req.flash("error", "Invalid email or password");
     return res.redirect("/login");
   }
 
